d7147228d19b351ed2d63effbe944942f3e77392
"Agustio" <"agustio719@gmail.com">
2022-06-30 20:04:20 +0700

    interaksi dengan contract lain

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -1,16 +1,27 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT 
+
 pragma solidity ^0.8.0;
 
-// mengimport contract dari file terpisah
-import "./SimpleStorage.sol";
+import "./SimpleStorage.sol"; 
 
-contract StorageFactory{
-
+contract StorageFactory {
+    
     SimpleStorage[] public simpleStorageArray;
     
     function createSimpleStorageContract() public {
         SimpleStorage simpleStorage = new SimpleStorage();
         simpleStorageArray.push(simpleStorage);
     }
-      
+    
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+        // Address 
+        // ABI 
+        // SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).store(_simpleStorageNumber);
+        simpleStorageArray[_simpleStorageIndex].store(_simpleStorageNumber);
+    }
+    
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+        // return SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).retrieve();
+        return simpleStorageArray[_simpleStorageIndex].retrive();
+    }
 }
